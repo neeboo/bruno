033e68593ab9e7d38a415b66f59cca3a2f13080d
neeboo <ghostcorn@gmail.com>
2021-12-10 18:33:50 +0800

    wip(null-safe):fix configs

--- lib/src/theme/configs/brn_tag_config.dart
+++ lib/src/theme/configs/brn_tag_config.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:bruno/src/theme/base/brn_base_config.dart';
+import 'package:bruno/src/theme/base/brn_default_config_utils.dart';
 import 'package:bruno/src/theme/base/brn_text_style.dart';
 import 'package:bruno/src/theme/brn_theme_configurator.dart';
 import 'package:bruno/src/theme/configs/brn_common_config.dart';
@@ -9,65 +10,83 @@ import 'package:bruno/src/theme/configs/brn_common_config.dart';
 class BrnTagConfig extends BrnBaseConfig {
   /// tag文本样式
   /// default TextStyle(fontSize: [BrnCommonConfig.fontSizeCaption],color: [BrnCommonConfig.colorTextBase])
-  BrnTextStyle tagTextStyle;
+  late BrnTextStyle tagTextStyle;
 
   /// tag选中文本样式
   /// default TextStyle(fontWeight: FontWeight.w600,fontSize: [BrnCommonConfig.fontSizeCaption],color: [BrnCommonConfig.brandPrimary])
-  BrnTextStyle selectTagTextStyle;
+  late BrnTextStyle selectTagTextStyle;
 
   /// 标签背景色
   /// default [BrnCommonConfig.fillBody]
-  Color tagBackgroundColor;
+  late Color tagBackgroundColor;
 
   /// 选中背景色
   /// default [BrnCommonConfig.brandPrimary]
-  Color selectedTagBackgroundColor;
+  late Color selectedTagBackgroundColor;
 
   /// 标签圆角
   /// default value is [BrnCommonConfig.radiusXs]
-  double tagRadius;
+  late double tagRadius;
 
   /// 标签高度，跟 fixWidthMode 无关
   /// default value is 34
-  double tagHeight;
+  late double tagHeight;
 
   /// 标签宽度，且仅在组件设置了固定宽度（fixWidthMode 为 true）时才生效
   /// default value is 75
-  double tagWidth;
+  late double tagWidth;
 
   /// 标签最小宽度
   /// default value is 75
-  double tagMinWidth;
+  late double tagMinWidth;
 
   BrnTagConfig(
-      {this.tagTextStyle,
-      this.selectTagTextStyle,
-      this.tagRadius,
-      this.tagBackgroundColor,
-      this.selectedTagBackgroundColor,
-      this.tagHeight,
-      this.tagWidth,
-      this.tagMinWidth,
+      {BrnTextStyle? tagTextStyle,
+      BrnTextStyle? selectTagTextStyle,
+      Color? tagBackgroundColor,
+      Color? selectedTagBackgroundColor,
+      double? tagRadius,
+      double? tagHeight,
+      double? tagWidth,
+      double? tagMinWidth,
       String configId = BrnThemeConfigurator.GLOBAL_CONFIG_ID})
-      : super(configId: configId);
+      : super(configId: configId) {
+    this.tagTextStyle = tagTextStyle ??
+        BrnTextStyle(
+            color: BrnDefaultConfigUtils.defaultCommonConfig.colorTextBase,
+            fontSize: BrnDefaultConfigUtils.defaultCommonConfig.fontSizeCaption);
+    this.selectTagTextStyle = selectTagTextStyle ??
+        BrnTextStyle(
+            fontWeight: FontWeight.w600,
+            color: BrnDefaultConfigUtils.defaultCommonConfig.brandPrimary,
+            fontSize: BrnDefaultConfigUtils.defaultCommonConfig.fontSizeCaption);
+    this.tagBackgroundColor =
+        tagBackgroundColor ?? BrnDefaultConfigUtils.defaultCommonConfig.fillBody;
+    this.selectedTagBackgroundColor =
+        selectedTagBackgroundColor ?? BrnDefaultConfigUtils.defaultCommonConfig.brandPrimary;
+    this.tagRadius = tagRadius ?? BrnDefaultConfigUtils.defaultCommonConfig.radiusXs;
+    this.tagHeight = tagHeight ?? 34;
+    this.tagWidth = tagWidth ?? 75;
+    this.tagMinWidth = tagMinWidth ?? 75;
+  }
 
   @override
-  void initThemeConfig(String configId, {BrnCommonConfig currentLevelCommonConfig}) {
+  void initThemeConfig(String configId, {BrnCommonConfig? currentLevelCommonConfig}) {
     super.initThemeConfig(configId, currentLevelCommonConfig: currentLevelCommonConfig);
 
     /// 用户全局组件配置
     BrnTagConfig tagConfig = BrnThemeConfigurator.instance.getConfig(configId: configId).tagConfig;
 
-    this.tagHeight ??= tagConfig.tagHeight;
+    this.tagHeight = tagConfig.tagHeight;
 
-    this.tagWidth ??= tagConfig.tagWidth;
-    this.tagMinWidth ??= tagConfig.tagMinWidth;
+    this.tagWidth = tagConfig.tagWidth;
+    this.tagMinWidth = tagConfig.tagMinWidth;
 
-    this.tagRadius ??= commonConfig.radiusXs;
+    this.tagRadius = commonConfig.radiusXs;
 
-    this.tagBackgroundColor ??= commonConfig.fillBody;
+    this.tagBackgroundColor = commonConfig.fillBody;
 
-    this.selectedTagBackgroundColor ??= commonConfig.brandPrimary;
+    this.selectedTagBackgroundColor = commonConfig.brandPrimary;
 
     this.tagTextStyle = tagConfig.tagTextStyle.merge(
         BrnTextStyle(color: commonConfig.colorTextBase, fontSize: commonConfig.fontSizeCaption)
@@ -79,14 +98,14 @@ class BrnTagConfig extends BrnBaseConfig {
   }
 
   BrnTagConfig copyWith(
-      {BrnTextStyle textStyle,
-      BrnTextStyle selectTextStyle,
-      double radius,
-      Color backgroundColor,
-      Color selectedBackgroundColor,
-      double height,
-      double width,
-      double tagMinWidth}) {
+      {BrnTextStyle? textStyle,
+      BrnTextStyle? selectTextStyle,
+      double? radius,
+      Color? backgroundColor,
+      Color? selectedBackgroundColor,
+      double? height,
+      double? width,
+      double? tagMinWidth}) {
     return BrnTagConfig(
         tagTextStyle: textStyle ?? this.tagTextStyle,
         selectTagTextStyle: selectTextStyle ?? this.selectTagTextStyle,
@@ -100,9 +119,9 @@ class BrnTagConfig extends BrnBaseConfig {
 
   BrnTagConfig merge(BrnTagConfig other) {
     return copyWith(
-        textStyle: this.tagTextStyle?.merge(other.tagTextStyle) ?? other.tagTextStyle,
+        textStyle: this.tagTextStyle.merge(other.tagTextStyle) ?? other.tagTextStyle,
         selectTextStyle:
-            this.selectTagTextStyle?.merge(other.selectTagTextStyle) ?? other.selectTagTextStyle,
+            this.selectTagTextStyle.merge(other.selectTagTextStyle) ?? other.selectTagTextStyle,
         radius: other.tagRadius,
         backgroundColor: other.tagBackgroundColor,
         selectedBackgroundColor: other.selectedTagBackgroundColor,
